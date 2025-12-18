a41c15ad09e1bf28362bccf0020683f3fe932bbc
ManuelLeitenbauer <leitenbauermanuel1@gmail.com>
2025-12-18 12:05:09 +0000

    Test

--- foo.c
+++ foo.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
-
+#include <math.h>
 int main(void)
 {
     printf("%s\n", "Hallo Welt!");
+
+    // Aufgabe 1: Weihnachtskekse
+
+   float a = 10;
+   int b = 5;
+    scanf("x = number_biscuits , y = diameter_bisvuit");
+    scanf("pow(%f, 2)* M_PI = %f");
+    printf("Gib die Gesammt Fläche aus" ,a = 10,b = 5);
+    printf("pow(%f, 2)* M_PI");
+
+
+  
+
+
+
+
+
+
+
+
+
+    // Aufgabe 2: Christbaum schmuck
+
+    
+    float x = 1.50;
+    float y = 4.90;
+
+    scanf("x = 1.50, y = 4.90");
+
+
+
+
+
+
+
+
+
+
+
 }
